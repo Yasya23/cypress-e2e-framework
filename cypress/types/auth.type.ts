88d5fd233f081533367945dda859799d9626ee07
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2026-08-24 20:26:12 +0300

    refactor(auth type): Delete unused interfaces

--- cypress/types/auth.type.ts
+++ cypress/types/auth.type.ts
@@ -7,25 +7,6 @@ export interface AuthResponse {
   access_token: string;
 }
 
-export interface RegisterApiAddress {
-  city: string;
-  country: string;
-  postal_code: string;
-  house_number: string;
-  street: string;
-  state: string;
-}
-
-export interface RegisterApiData {
-  first_name: string;
-  last_name: string;
-  dob: string;
-  address: RegisterApiAddress;
-  phone_number: string;
-  email: string;
-  password: string;
-}
-
 export interface RegisterData {
   firstName: string;
   lastName: string;
