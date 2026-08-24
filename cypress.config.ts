3876bcebaa94aa90e16b1b129e2d59225407554c
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2026-08-24 20:23:12 +0300

    chore (configs): Add tsconfig and cypress configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,20 @@
-const { defineConfig } = require("cypress");
+require('dotenv').config();
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
   e2e: {
+    baseUrl: process.env.BASE_URL,
+    env: {
+      USER_EMAIL: process.env.USER_EMAIL,
+      USER_PASSWORD: process.env.USER_PASSWORD,
+    },
+    expose: {
+      BASE_API_URL: process.env.BASE_API_URL,
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      console.log('baseUrl:', config.baseUrl);
+      return config;
     },
   },
 });
